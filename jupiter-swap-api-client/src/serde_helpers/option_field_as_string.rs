43d9b88e0ff7488d6c7684d8785da5208323f7b2
Andrew Tan <andtan91@gmail.com>
2024-11-04 20:56:32 -0500

    Allow deserialize

--- jupiter-swap-api-client/src/serde_helpers/option_field_as_string.rs
+++ jupiter-swap-api-client/src/serde_helpers/option_field_as_string.rs
@@ -1,4 +1,10 @@
-use {serde::Serialize, serde::Serializer};
+use {
+    serde::{de, Deserializer, Serializer},
+    serde::{Deserialize, Serialize},
+    std::str::FromStr,
+    std::fmt::Display,
+};
+
 
 pub fn serialize<T, S>(t: &Option<T>, serializer: S) -> Result<S::Ok, S::Error>
 where
@@ -9,5 +15,26 @@ where
         t.to_string().serialize(serializer)
     } else {
         serializer.serialize_none()
+    }
+}
+
+pub fn deserialize<'de, T, D>(deserializer: D) -> Result<Option<T>, D::Error>
+where
+    T: FromStr,
+    D: Deserializer<'de>,
+    // Use Display for better error messages
+    <T as FromStr>::Err: Display,
+{
+    // Attempt to deserialize an Option<String>
+    let opt_s: Option<String> = Option::deserialize(deserializer)?;
+    match opt_s {
+        Some(s) => {
+            // Attempt to parse the string into T
+            match s.parse::<T>() {
+                Ok(value) => Ok(Some(value)),
+                Err(e) => Err(de::Error::custom(format!("Parse error: {}", e))),
+            }
+        }
+        None => Ok(None),
     }
 }
